4e0f97332ee18711474cddcc669798b724555875
v.zhabin <v.zhabin@focus.bi>
2024-03-10 14:39:13 +0300

    feat: Добавлен первый инкремент

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,54 @@
 package main
 
-func main() {}
+import (
+	b64 "encoding/base64"
+	"io"
+	"net/http"
+	"strings"
+)
+
+func mainPOST(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		body, _ := io.ReadAll(r.Body)
+
+		w.WriteHeader(201)
+		w.Header().Set("content-type", "text/plain")
+		w.Write([]byte(body))
+
+		//sEnc := b64.StdEncoding.EncodeToString([]byte(shortId))
+		// пока установим ответ-заглушку, без проверки ошибок
+		//_, _ = w.Write([]byte(`
+		//{
+		//	"response":
+		//`))
+	} else {
+		w.WriteHeader(400)
+	}
+}
+
+func mainGET(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		//host := r.Host
+		shortId := strings.Split(r.URL.Path, "/")[1]
+		decode, _ := b64.StdEncoding.DecodeString(shortId)
+		w.Header().Set("content-type", "text/plain")
+		w.Header().Set("Location", string(decode))
+		//sEnc := b64.StdEncoding.EncodeToString([]byte(shortId))
+		//fmt.Fprintf(w, "Hello, %q", sEnc)
+		w.WriteHeader(307)
+		return
+	} else {
+		w.WriteHeader(400)
+	}
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/{id}`, mainGET)
+	mux.HandleFunc(`/`, mainPOST)
+
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
